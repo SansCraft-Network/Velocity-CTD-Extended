769e331d3832e5ab8443c14755f32c53cc45501f
R00tB33rMan <rootb33rman@gmail.com>
2025-04-30 19:56:17 -0400

    [ci skip] Upstream fix

--- proxy/src/main/java/com/velocitypowered/proxy/protocol/ProtocolUtils.java
+++ proxy/src/main/java/com/velocitypowered/proxy/protocol/ProtocolUtils.java
@@ -93,15 +93,15 @@ public enum ProtocolUtils {
           .legacyHoverEventSerializer(NBTLegacyHoverEventSerializer.get())
           .options(
               OptionSchema.globalSchema().stateBuilder()
-                  // after 1.16
-                  .value(JSONOptions.EMIT_RGB, Boolean.TRUE)
-                  .value(JSONOptions.EMIT_HOVER_EVENT_TYPE, JSONOptions.HoverEventValueMode.CAMEL_CASE)
-                  .value(JSONOptions.EMIT_CLICK_EVENT_TYPE, JSONOptions.ClickEventValueMode.CAMEL_CASE)
-                  // after 1.20.3
-                  .value(JSONOptions.EMIT_COMPACT_TEXT_COMPONENT, Boolean.TRUE)
-                  .value(JSONOptions.EMIT_HOVER_SHOW_ENTITY_ID_AS_INT_ARRAY, Boolean.TRUE)
-                  .value(JSONOptions.VALIDATE_STRICT_EVENTS, Boolean.TRUE)
-                  .build()
+              // after 1.16
+              .value(JSONOptions.EMIT_RGB, Boolean.TRUE)
+              .value(JSONOptions.EMIT_HOVER_EVENT_TYPE, JSONOptions.HoverEventValueMode.CAMEL_CASE)
+              .value(JSONOptions.EMIT_CLICK_EVENT_TYPE, JSONOptions.ClickEventValueMode.CAMEL_CASE)
+              // after 1.20.3
+              .value(JSONOptions.EMIT_COMPACT_TEXT_COMPONENT, Boolean.TRUE)
+              .value(JSONOptions.EMIT_HOVER_SHOW_ENTITY_ID_AS_INT_ARRAY, Boolean.TRUE)
+              .value(JSONOptions.VALIDATE_STRICT_EVENTS, Boolean.TRUE)
+              .build()
           )
           .build();
   private static final GsonComponentSerializer MODERN_SERIALIZER =

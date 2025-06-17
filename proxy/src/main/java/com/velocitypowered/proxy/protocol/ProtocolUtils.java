b84478c21922b20a5dac7c9570af2e501b51e748
R00tB33rMan <rootb33rman@gmail.com>
2025-06-17 14:55:06 -0400

    Apply stable unmerged upstream PRs

--- proxy/src/main/java/com/velocitypowered/proxy/protocol/ProtocolUtils.java
+++ proxy/src/main/java/com/velocitypowered/proxy/protocol/ProtocolUtils.java
@@ -71,6 +71,8 @@ public enum ProtocolUtils {
               .value(JSONOptions.EMIT_COMPACT_TEXT_COMPONENT, Boolean.FALSE)
               .value(JSONOptions.EMIT_HOVER_SHOW_ENTITY_ID_AS_INT_ARRAY, Boolean.FALSE)
               .value(JSONOptions.VALIDATE_STRICT_EVENTS, Boolean.FALSE)
+              // before 1.21.5
+              .value(JSONOptions.EMIT_CHANGE_PAGE_CLICK_EVENT_PAGE_AS_STRING, Boolean.TRUE)
               .build()
           )
           .build();
@@ -88,6 +90,8 @@ public enum ProtocolUtils {
               .value(JSONOptions.EMIT_COMPACT_TEXT_COMPONENT, Boolean.FALSE)
               .value(JSONOptions.EMIT_HOVER_SHOW_ENTITY_ID_AS_INT_ARRAY, Boolean.FALSE)
               .value(JSONOptions.VALIDATE_STRICT_EVENTS, Boolean.FALSE)
+              // before 1.21.5
+              .value(JSONOptions.EMIT_CHANGE_PAGE_CLICK_EVENT_PAGE_AS_STRING, Boolean.TRUE)
               .build()
           )
           .build();
@@ -105,6 +109,8 @@ public enum ProtocolUtils {
               .value(JSONOptions.EMIT_COMPACT_TEXT_COMPONENT, Boolean.TRUE)
               .value(JSONOptions.EMIT_HOVER_SHOW_ENTITY_ID_AS_INT_ARRAY, Boolean.TRUE)
               .value(JSONOptions.VALIDATE_STRICT_EVENTS, Boolean.TRUE)
+              // before 1.21.5
+              .value(JSONOptions.EMIT_CHANGE_PAGE_CLICK_EVENT_PAGE_AS_STRING, Boolean.TRUE)
               .build()
           )
           .build();
@@ -123,6 +129,7 @@ public enum ProtocolUtils {
               // after 1.21.5
               .value(JSONOptions.EMIT_HOVER_SHOW_ENTITY_KEY_AS_TYPE_AND_UUID_AS_ID, Boolean.FALSE)
               .value(JSONOptions.VALIDATE_STRICT_EVENTS, Boolean.TRUE)
+              .value(JSONOptions.EMIT_CHANGE_PAGE_CLICK_EVENT_PAGE_AS_STRING, Boolean.FALSE)
               .build()
           )
           .build();
